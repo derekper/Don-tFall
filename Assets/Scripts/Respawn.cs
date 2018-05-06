fbb4810aff427d96f9b38dc0d3394b02b3e246d7
Unknown <derek.pereira@stonybrook.edu>
2018-05-06 19:46:23 -0400

    The New Repo

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+    //creates entry fields inside inspector
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FallCounter.falls += 1;
+        //resets players position to respawn point
+        player.transform.position = respawnpoint.transform.position;
+    }
+
+}
